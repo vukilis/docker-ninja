a6c1da66bd00b6becd3ffa9dba5f85debbab2620
vukilis <vuk.lekic@vukilis.com>
2026-06-10 18:10:31 +0200

    updated

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // Use standalone output for optimized production builds
-  // output: 'standalone',
+  // output: 'export',
   // Rewrites for SPA routing
   // async rewrites() {
   //   return [
@@ -11,9 +11,11 @@ const nextConfig: NextConfig = {
   //     { source: '/sponsoring', destination: '/' },
   //     { source: '/containers', destination: '/' },
   //     { source: '/categories', destination: '/' },
+  //     { source: '/docs', destination: '/' },
   //     { source: '/app/:slug', destination: '/?preview=:slug' },
   //   ];
   // },
+  // allowedDevOrigins: ['test.domain.com']
 };
 
 export default nextConfig;
