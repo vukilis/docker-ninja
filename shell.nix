c23279ecb5b2bda0d34107c038fd34c1eefcfa4a
vukilis <vuk.lekic@vukilis.com>
2026-06-19 22:38:05 +0200

    feat(shell.nix): switch to unstable nixpkgs and update pnpm package

--- shell.nix
+++ shell.nix
@@ -1,15 +1,16 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz") {} }:
 
 pkgs.mkShell {
   name = "docker-ninja-web-env";
 
   buildInputs = with pkgs; [
     nodejs_22
-    nodePackages.pnpm # Faster and more efficient than npm
+    pnpm # Faster and more efficient than npm
   ];
 
   shellHook = ''
     echo "DockerNinja Web Environment Loaded."
     echo "Node version: $(node -v)"
+    echo "pnpm version: $(pnpm -v)"
   '';
 }
